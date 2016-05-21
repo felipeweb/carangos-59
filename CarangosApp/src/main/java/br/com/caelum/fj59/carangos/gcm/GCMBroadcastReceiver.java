b346a71be377967cda41a4c20b6321c90db0bedf
Felipe Oliveira <felipeweb.programador@gmail.com>
2016-05-21 12:19:53 -0300

    6.3

--- CarangosApp/src/main/java/br/com/caelum/fj59/carangos/gcm/GCMBroadcastReceiver.java
+++ CarangosApp/src/main/java/br/com/caelum/fj59/carangos/gcm/GCMBroadcastReceiver.java
@@ -1,8 +1,13 @@
 package br.com.caelum.fj59.carangos.gcm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import br.com.caelum.fj59.carangos.R;
+import br.com.caelum.fj59.carangos.activity.MainActivity;
 import br.com.caelum.fj59.carangos.infra.MyLog;
 
 /**
@@ -13,5 +18,23 @@ public class GCMBroadcastReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		MyLog.i("Chegou a mensagem do GCM!");
+		String mensagem = (String) intent.getExtras().getSerializable("message");
+		Intent irParaLeilao = new Intent(context, MainActivity.class);
+		PendingIntent acaoPendente = PendingIntent
+				.getActivity(context, 0, irParaLeilao,
+						PendingIntent.FLAG_CANCEL_CURRENT);
+		irParaLeilao.putExtra("idDaNotificacao", Constantes.ID_NOTIFICACAO);
+		Notification notificacao = new Notification.Builder(context)
+				.setContentTitle("Um novo leilao começou!")
+				.setContentText("Leilao: " + mensagem)
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setContentIntent(acaoPendente)
+				.setAutoCancel(true)
+				.build();
+		NotificationManager manager =
+				(NotificationManager)
+						context.getSystemService
+								(Context.NOTIFICATION_SERVICE);
+		manager.notify(Constantes.ID_NOTIFICACAO, notificacao);
 	}
 }
